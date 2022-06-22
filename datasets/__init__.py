1d7975f1b5f8ec0040f191c1f5cc0fb5b4b7823a
Aleksandr Mikhailov <avidclam+statagain@gmail.com>
2022-06-22 13:28:16 +0300

    Email change

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -5,10 +5,10 @@ with code required to etl them into a single sqlite database.
 
 __title__ = __name__
 __description__ = __doc__.replace('\n', ' ').replace('\r', '').strip()
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Aleksandr Mikhailov'
-__author_email__ = 'am@statagain.ru'
-__copyright__ = '2021, Aleksandr Mikhailov'
+__author_email__ = 'avidclam+statagain@gmail.com'
+__copyright__ = '2022, Aleksandr Mikhailov'
 
 from .core import init_db
 from .uci_ml_00579 import load_MI_complications
