9437ff0a5ec7ccc4d9ebe19aaf2bf2a1fbb3e576
Aleksandr Mikhailov <avidclam+statagain@gmail.com>
2022-07-07 23:07:40 +0300

    Use generator instead of a list

--- build_db.py
+++ build_db.py
@@ -6,8 +6,10 @@ import datasets
 DB_PATH = Path('target') / 'statagain.db'
 
 if __name__ == '__main__':
-    loaders = [func for name, func in getmembers(datasets, isfunction)
-               if name.startswith('load_')]
+    # All functions exported from datasets whose name start with 'load_"
+    # are considered dataset loaders and called to set up database.
+    loaders = (func for name, func in getmembers(datasets, isfunction)
+               if name.startswith('load_'))
     con = sqlite3.connect(DB_PATH)
     datasets.init_db(con)
     for loader in loaders:
